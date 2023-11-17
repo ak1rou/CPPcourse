f7d430e8e72bb55beb673c5e37134dc4e091dd7b
ak1rou <van.ban277353@gmail.com>
2023-11-17 23:26:15 +0200

    HW6 first task

---
+++ HW6/6hw.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+    
+    double a1, d;
+    int n;
+
+    std::cout << "Enter the first element of the arithmetic progression: ";
+    std::cin >> a1;
+
+    std::cout << "Enter the common difference of the arithmetic progression: ";
+    std::cin >> d;
+
+    std::cout << "Enter the number of the last element of the arithmetic progression: ";
+    std::cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        double currentElement = a1 + i * d;
+        std::cout << currentElement << " ";
+    }
+
+    return 0;
+}
