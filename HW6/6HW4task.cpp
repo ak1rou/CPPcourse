504b3da51b69749134fa0ae97232525db44cf57a
ak1rou <van.ban277353@gmail.com>
2023-11-18 00:12:45 +0200

    6HW 4task

---
+++ HW6/6HW4task.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+int main() {
+    int size;
+
+    std::cout << "Enter the size: ";
+    std::cin >> size;
+
+    std::cout << "a." << std::endl;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size - i; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "b." << std::endl;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < i; ++j) {
+            std::cout << " ";
+        }
+        for (int k = 0; k < size - i; ++k) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "c." << std::endl;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "d." << std::endl;
+    for (int i = 0; i < size; ++i) {
+       
+        for (int j = 0; j < i; ++j) {
+            std::cout << " ";
+        }
+
+        for (int k = 0; k < size; ++k) {
+            std::cout << "*";
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::cout << "e." << std::endl;
+    for (int i = 1; i <= size; ++i) {
+        for (int j = 1; j <= size - i; ++j) {
+            std::cout << " ";
+        }
+        for (int k = 1; k <= i; ++k) {
+            std::cout << (k % 2);
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
