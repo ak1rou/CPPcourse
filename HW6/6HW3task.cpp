e719b884cf486416532d992d192d3d193e6d8171
ak1rou <van.ban277353@gmail.com>
2023-11-29 00:59:07 +0200

    update hw6 task3

--- HW6/6HW3task.cpp
+++ HW6/6HW3task.cpp
@@ -2,17 +2,18 @@
 
 int main() {
     int n;
-  
+
     std::cout << "Enter a positive integer: ";
     std::cin >> n;
- 
+
     if (n < 0) {
         std::cout << "Please enter a positive integer." << std::endl;
-        return 1;  
+        return 1;
     }
- 
+
     unsigned long long factorial = 1;
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 2; i <= n; ++i) {
+
         factorial *= i;
     }
 
@@ -20,4 +21,3 @@ int main() {
 
     return 0;
 }
-// при введенні значення >65 видає 0 в результаті (наскільки я зрозумів через переповенення лонг лонг) і я не знайшов як це пофіксити(( 
