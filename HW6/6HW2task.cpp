492d550503096b216a7f986f9ee3973fb695f7fe
ak1rou <van.ban277353@gmail.com>
2023-11-29 01:02:29 +0200

    update hw6 task2

--- HW6/6HW2task.cpp
+++ HW6/6HW2task.cpp
@@ -3,28 +3,18 @@
 int main() {
     int n;
 
-    
     std::cout << "Enter the number of the Fibonacci sequence element: ";
     std::cin >> n;
 
-    
-    int fib1 = 1, fib2 = 1, fibN = 0;
+    int fib1 = 1, fib2 = 1, fibN = 1;
 
-    if (n == 1 || n == 2) 
-    {
-        fibN = 1;
+    for (int i = 3; i <= n; ++i) {
+        fibN = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fibN;
     }
-    else
-    {
-        for (int i = 3; i <= n; ++i) {
-            fibN = fib1 + fib2;
-            fib1 = fib2;
-            fib2 = fibN;
-        }
-    }
+
     std::cout << "The Fibonacci sequence element at position " << n << " is: " << fibN << std::endl;
-    
-   
 
     return 0;
 }
