c725e4d8ca4d40c21a26a8810c37ea4290a031b9
ak1rou <van.ban277353@gmail.com>
2023-12-13 13:54:25 +0200

    Update functions.cpp

--- HW9/HW9/functions.cpp
+++ HW9/HW9/functions.cpp
@@ -22,10 +22,7 @@ void toUppercase(char str[])
 
     for (int i = 0; i < length; ++i)
     {
-        if (str[i] >= 'a' && str[i] <= 'z')
-        {
-            str[i] = toupper(str[i]);
-        }
+        str[i] = toupper(str[i]);
     }
 }
 
@@ -36,20 +33,16 @@ bool isPalindrome(const char str[])
     int start = 0;
     int end = length - 1;
 
-    while (start < end)
-    {
-        while (!isalnum(str[start]) && start < end) 
-        {
+    while (start < end) {
+        while (!isalnum(str[start]) && start < end) {
             start++;
         }
 
-        while (!isalnum(str[end]) && start < end)
-        {
+        while (!isalnum(str[end]) && start < end) {
             end--;
         }
 
-        if (tolower(str[start]) != tolower(str[end]))
-        {
+        if (str[start] != str[end]) {
             return false;
         }
 
@@ -65,18 +58,14 @@ void parseStringLetters(const char str[], int& vowelsCount, int& consonantsCount
     vowelsCount = 0;
     consonantsCount = 0;
 
-    for (int i = 0; str[i] != '\0'; ++i)
-    {
-        char currentChar = tolower(str[i]);
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (isalpha(str[i])) {
+            char currentChar = tolower(str[i]);
 
-        if (isalpha(currentChar))
-        {
-            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u')
-            {
+            if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u') {
                 vowelsCount++;
             }
-            else
-            {
+            else {
                 consonantsCount++;
             }
         }
@@ -85,16 +74,13 @@ void parseStringLetters(const char str[], int& vowelsCount, int& consonantsCount
 
 bool isEqual(const char str1[], const char str2[])
 {
-    int i = 0;
+    size_t i = 0;
 
-    while (str1[i] != '\0' && str2[i] != '\0')
-    {
-        if (str1[i] != str2[i])
-        {
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
             return false;
         }
         i++;
     }
-
     return str1[i] == '\0' && str2[i] == '\0';
 }
