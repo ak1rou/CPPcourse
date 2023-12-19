72b08fb9706ca64183fc768046eadffe5940130b
ak1rou <van.ban277353@gmail.com>
2023-12-20 01:30:12 +0200

    HW13

--- HW13/HW13/HW13/funcs.h
+++ HW13/HW13/HW13/funcs.h
@@ -1,31 +1,32 @@
-//#pragma once
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <iostream>
-//#include <fstream>
-//#include <ctime>
-//#include <cstdlib>
-//#include <vector>
-//#include <algorithm>
-//#include <map>
-//
-//std::vector<std::string> wordDatabase = { "biker", "loser", "crown", "party", "disco", "mommy", "daddy", "grand", "sharp", "prism", "month", "hello", "cocon", "speak" };
-//const std::string WORD_DATABASE_FILE = "word_database.txt";
-//const std::string WORD_OF_THE_DAY_FILE = "word_of_the_day_status.txt";
-//
-//std::vector<std::string> loadWordDatabase();
-//
-//std::string generateWordOfTheDay(const std::vector<std::string>& wordDatabase);
-//
-//std::string getRandomWord(const std::vector<std::string>& wordDatabase);
-//
-//bool checkGuess(const std::string& secretWord, const std::string& guessedWord);
-//
-//void saveWordOfTheDayStatus(bool wordOfTheDayGuessed, const std::string& todayDate);
-//
-//bool loadWordOfTheDayStatus(std::string& storedDate);
-//
-//bool isSameDay(const std::string& storedDate, const std::string& currentDate);
-//
-//std::string getTodayDate();
-//
-//void playWordleGame(const std::string& secretWord, bool& wordOfTheDayGuessed, std::string& storedDate);
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+
+extern const std::string WORD_DATABASE_FILE;
+extern const std::string WORD_OF_THE_DAY_FILE;
+extern std::vector<std::string> wordDatabase;
+
+std::vector<std::string> loadWordDatabase();
+
+void saveWordDatabase(const std::vector<std::string>& words);
+
+std::string getRandomWord(const std::vector<std::string>& wordDatabase);
+
+void saveWordOfTheDayStatus(bool wordOfTheDayGuessed, const std::string& todayDate);
+
+bool loadWordOfTheDayStatus(bool& wordOfTheDayGuessed, std::string& storedDate);
+
+std::string getTodayDate();
+
+bool checkGuess(const std::string& secretWord, const std::string& guessedWord, bool& wordOfTheDayGuessed, std::string& todayDate, int& attempts);
+
+bool isSameDay(const std::string& storedDate, const std::string& currentDate);
+
+void playWordleGame(const std::string& secretWord, bool& wordOfTheDayGuessed, std::string& storedDate);
