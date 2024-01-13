a8f99041b9b2e9db2b87cb381c71d6f7f0a929e6
ak1rou <van.ban277353@gmail.com>
2024-01-13 14:09:11 +0200

    updated

--- HW16/HW16/HW16.cpp
+++ HW16/HW16/HW16.cpp
@@ -4,13 +4,7 @@
 #include "Player.h"
 #include "Team.h"
 #include "Rectangle.h"
-
-void displayMainMenu();
-void displayPlayerInfo(const Player& player);
-PlayerClass choosePlayerClass();
-Weapon* createWeapon();
-void handleAddPlayer(std::vector<Player>& players, Team& direTeam, Team& radiantTeam);
-void handleRemovePlayer(std::vector<Player>& players, Team& direTeam, Team& radiantTeam);
+#include "mainmenu.h"
 
 int main() 
 {
